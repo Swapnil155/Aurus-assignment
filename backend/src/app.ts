5977f9ecfb35556566bd19116ccd067e49e67322
Swapnil <swapnilbendal155@gmail.com>
2025-02-09 12:25:55 +0530

    add cookie-parser and cors middleware with configuration for handling requests

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,18 @@
 import requestId from 'request-ip';
 import express, { Application, NextFunction, Request, Response } from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
 import config from './config/config';
 import morgan from './config/morgan';
-import path from 'path';
 import logger from './config/logger';
 import error from './middleware/error';
 import routes from './routes';
 import ApiError from './utils/helper/ApiError';
+import { corsOptions } from './config/cors';
 
 class App {
+	
 	private app: Application;
 
 	constructor() {
@@ -18,37 +22,65 @@ class App {
 		this.initializeErrorHandling();
 	}
 
+
 	private initializeMiddlewares(): void {
+
 		if (config.env !== 'test') {
+
 			this.app.use(morgan.successHandler);
+
 			this.app.use(morgan.errorHandler);
+
 		}
+
+		this.app.use(cookieParser());
+
 		this.app.use(requestId.mw());
 
 		this.app.use(express.json());
+
 		this.app.use(express.urlencoded({ extended: true }));
+
+		this.app.use(cors(corsOptions));
+
+		this.app.options('*', cors(corsOptions));
+
 		this.app.use('/public', express.static(path.join(__dirname, '../public')));
+
 	}
 
+
 	private initializeRoutes(): void {
+
 		this.app.use('/api', routes);
+
 		// Define our routes
 		this.app.use((req: Request, res: Response, next: NextFunction) => {
+
 			next(new ApiError(404, 'Not found'));
+
 		});
+
 	}
+
 
 	private initializeErrorHandling(): void {
 		this.app.use(error.errorConverter);
 		this.app.use(error.errorHandler);
 	}
 
+
 	public listen(port: number): ReturnType<typeof this.app.listen> {
+
 		return this.app.listen(port, () => {
+
 			logger.info(`Server listening on port ${config.port}`);
+
 			logger.info(`Environment :- ${config.env}`);
+
 		});
 	}
+
 }
 
 export default new App();
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,40 +6,69 @@ import logger from './config/logger';
 
 let server: ReturnType<typeof app.listen>;
 
-AppDataSource.initialize()
+
+AppDataSource
+	.initialize()
 	.then(() => {
+
 		server = app.listen(config.port);
+
 		logger.info('Data Source has been initialized!');
+
 	})
 	.catch((err: Error) => {
+
 		console.log(err);
 
 		logger.error('Error during Data Source initialization', err);
+
 		process.exit(1);
+
 	});
 
+
 const exitHandler = () => {
+
 	if (server) {
+
 		server.close(() => {
+
 			logger.info('server closed');
+
 			process.exit(1);
+
 		});
+
 	} else {
+
 		process.exit(1);
+
 	}
+	
 };
+
 
 const unexpectedErrorHandler = (error: Error) => {
+
 	logger.error('Un-Expected Error: ', error);
+
 	exitHandler();
+
 };
 
+
 process.on('uncaughtException', unexpectedErrorHandler);
+
 process.on('unhandledRejection', unexpectedErrorHandler);
 
+
 process.on('SIGALRM', () => {
+
 	logger.info('SIGALRM signal received');
+
 	if (server) {
+
 		server.close();
+
 	}
 });
